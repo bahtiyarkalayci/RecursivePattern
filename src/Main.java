4e736d781b66eaf78fda0a4518dd4b731867ff4e
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-22 13:24:07 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
-public class Main {
+
+
+import java.util.Scanner;
+
+ class RecursivePattern {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Kullanıcıdan 'N' sayısını alıyoruz
+        Scanner input = new Scanner(System.in);
+        System.out.print("N Sayısı : ");
+        int number = input.nextInt();
+
+        //Recursive metodu çalıştırıyoruz ve çıktısını alıyoruz
+        System.out.print("Çıktısı : ");
+        pattern(pattern(number), number);
+
+        input.close();
+    }
+
+    //Paternde 'N' sayısından geriye doğru 'N-5' çıktı alan overloaded recursive metod
+    static int pattern(int n){
+        if (n <= 0){
+            return n;
+        }
+        System.out.print(n + " ");
+        return pattern(n-5);
+    }
+
+    //Paternde ileri doğru 'N+5' çıktı alan overloading recursive metod
+    static int pattern(int n, int i){
+        if (n > i){
+            return n;
+        }
+        System.out.print(n + " ");
+        return pattern(n+5, i);
     }
 }
